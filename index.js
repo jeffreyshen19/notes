94de5340477fe8eb58939fbdeb35677b32b33caa
jeffreyshen19 <jshen@andover.edu>
2019-02-19 23:59:12 -0500

    added framework to automatically add notes

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ rl.prompt();
 
 rl.on('line', function (cmd) {
   input.push(cmd);
+  rl.prompt();
 });
 
 rl.on('close', function (cmd) {
-  process.exit(0);
-
   var date = new Date();
   var date_name = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + "-" + (date.getHours() % 12 == 0 ? 12 : date.getHours() % 12) + ":" + date.getMinutes() + (date.getHours < 12  ? "AM" : "PM");
 
-  var template = "extends ../layout.pug\nblock note\n\tp ";
+  var template = "-var note = '" + input.join("<br>") + "'\ninclude ../layout.pug";
 
-  console.log(template + input.join("\n"));
+  fs.writeFileSync("./drafts/" + date_name + ".pug", template);
 
-  fs.writeFileSync("./drafts/" + date_name + ".pug", template + input.join("\n"));
+  process.exit(0);
 });
